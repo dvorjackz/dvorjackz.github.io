e85ab7c0fab543474ab5e4e3861b4ac551ceae9a
Jack Zhang <hijackz337@gmail.com>
2019-08-12 13:49:35 -0700

    Fixed flight pattern, keeping it simple by not making it circle connect link

--- js/main.js
+++ js/main.js
@@ -14,15 +14,16 @@ $(function() {
     }
 
     // Plane starting position (out of view to the left of the view)
-    planeWidth = document.getElementById("target").clientWidth;
-    document.getElementById("target").style.left = (-((screenWidth - document.getElementsByClassName("full")[0].clientWidth)/2 + planeWidth)).toString() + "px";
+    planeWidth = document.getElementById("plane").clientWidth;
+    document.getElementById("target").style.left = (-planeWidth).toString() + "px";
 
+    // Set vertical position of work experience section
     var frontDivHeight = document.getElementById('front').clientHeight;
     document.getElementById("trigger1").style.top += (frontDivHeight/2 + delay).toString() + "px";
 
     var controller = new ScrollMagic.Controller();
 
-    var tween = TweenMax.staggerFromTo(".animate4", 1, {y: screenHeight*3/2}, {y: 0, ease: Back.easeOut.config(1)}, 0.5);
+    var tween = TweenMax.staggerFromTo(".staggerAnimate", 1, {y: screenHeight*3/2}, {y: 0, ease: Back.easeOut.config(1)}, 0.5);
 
     console.log(screenHeight/3);
 
@@ -31,36 +32,68 @@ $(function() {
         .addIndicators({name: "staggering"}) // add indicators (requires plugin)
         .addTo(controller);
   
+    // var flightpath = {
+    //     looping : {
+    //         curviness: 1.25,
+    //         autoRotate: true,
+    //         values: [
+    //                 // {x: 510,	y: 60},
+    //                 // {x: 620,	y: -60},
+    //                 // {x: 500,	y: -100},
+    //                 // {x: 380,	y: 20},
+    //                 // {x: 500,	y: 60},
+    //                 // {x: 580,	y: 20},
+    //                 // {x: 620,	y: 15}
+    //                 {x: screenWidth/2 - 200, y: 100},
+    //                 {x: screenWidth/2 + 250, y: -30},
+    //                 {x: screenWidth/2 - 200, y: -60},
+    //                 {x: screenWidth/2 + 150, y: -20},
+    //                 {x: screenWidth*3/2 + planeWidth, y: 600},
+    //             ]
+    //     }
+    // };
+
     var flightpath = {
+        entry : {
+            curviness: 1.25,
+            autoRotate: true,
+            values: [
+                    {x: 100,	y: -20},
+                    {x: 300,	y: 10}
+                ]
+        },
         looping : {
             curviness: 1.25,
             autoRotate: true,
             values: [
-                    // {x: 510,	y: 60},
-                    // {x: 620,	y: -60},
-                    // {x: 500,	y: -100},
-                    // {x: 380,	y: 20},
-                    // {x: 500,	y: 60},
-                    // {x: 580,	y: 20},
-                    // {x: 620,	y: 15}
-                    {x: screenWidth/2 - 200, y: 100},
-                    {x: screenWidth/2 + 250, y: -30},
-                    {x: screenWidth/2 - 200, y: -60},
-                    {x: screenWidth/2 + 150, y: -20},
-                    {x: screenWidth*3/2 + planeWidth, y: -200},
+                    {x: 510,	y: 60},
+                    {x: 620,	y: -60},
+                    {x: 500,	y: -100},
+                    {x: 380,	y: 20},
+                    {x: 500,	y: 60},
+                    {x: 580,	y: 20},
+                    {x: 620,	y: 15}
+                ]
+        },
+        leave : {
+            curviness: 1.25,
+            autoRotate: true,
+            values: [
+                    {x: 660,	y: 20},
+                    {x: 800,	y: 130},
+                    {x: $(window).width() + 300,	y: -100},
                 ]
         }
     };
 
     // create tween
     var tween = new TimelineMax()
-        // .add(TweenMax.to($("#plane"), 1.2, {css:{bezier:flightpath.entry}, ease:Power1.easeInOut}))
+        .add(TweenMax.to($("#plane"), 1.2, {css:{bezier:flightpath.entry}, ease:Power1.easeInOut}))
         .add(TweenMax.to($("#plane"), 2, {css:{bezier:flightpath.looping}, ease:Power1.easeInOut}))
-        // .add(TweenMax.to($("#plane"), 1, {css:{bezier:flightpath.leave}, ease:Power1.easeInOut}));
+        .add(TweenMax.to($("#plane"), 1, {css:{bezier:flightpath.leave}, ease:Power1.easeInOut}));
 
     // build scene
-    var scene0 = new ScrollMagic.Scene({triggerElement: "#trigger0", duration: 500, offset: 100})
-        .setPin("#target")
+    var scene0 = new ScrollMagic.Scene({triggerElement: "#trigger0", triggerHook: "onEnter", duration: 1000, offset: 100})
         .setTween(tween)
         .addIndicators({name: "airplane"}) // add indicators (requires plugin)
         .addTo(controller);

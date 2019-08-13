f8ca1ddb5f100ce4ac71e214cc836c9ae241b1eb
Jack Zhang <hijackz337@gmail.com>
2019-08-13 03:10:38 -0700

    v2 done. Need to add end page and icons/pictures

--- js/main.js
+++ js/main.js
@@ -10,43 +10,50 @@ $(function() {
         screenWidth = screen.width * ratio;
     }
 
-    // How much to offset each trigger location by
-    var delay = -100;
-
-    // Set vertical position of work experience section triggers
-    var frontDivHeight = document.getElementById('front').clientHeight;
-    document.getElementById("trigger1").style.top += (frontDivHeight/2 + delay).toString() + "px";
-    document.getElementById("trigger2").style.top += (frontDivHeight/2 + delay).toString() + "px";
-
     // Set up scroll magic
     var controller = new ScrollMagic.Controller();
 
     // Fade in the frontpage on startup
     var tween0 = TweenMax.fromTo("#front", 2, {autoAlpha: 0}, {autoAlpha: 1})
 
+    // (FRAMEWORK)
     section1Height = document.getElementById("section1").clientHeight;
     section2Height = document.getElementById("section2").clientHeight;
     section3Height = document.getElementById("section3").clientHeight;
+    section4Height = document.getElementById("section4").clientHeight;
     console.log(section3Height)
 
-    // The animations for each profile section (start y's start at 3/2 and increment by 1 for each section, e.g. 3/2, 5/2, 7/2 ...)
+    // (FRAMEWORK) The animations for each profile section (start y's start at 3/2 and increment by 1 for each section, e.g. 3/2, 5/2, 7/2 ...)
     var tween1 = TweenMax.staggerFromTo(".staggerAnimate1", 1, {y: screenHeight*3/2}, {y: 0, ease: Back.easeOut.config(0.75)}, 0.5);
     var tween2 = TweenMax.staggerFromTo(".staggerAnimate2", 1, {y: screenHeight*5/2}, {y: 0, ease: Back.easeOut.config(0.75)}, 0.5);
     var tween3 = TweenMax.staggerFromTo(".staggerAnimate3", 1, {y: screenHeight*7/2}, {y: 0, ease: Back.easeOut.config(0.75)}, 0.5);
+    var tween4 = TweenMax.staggerFromTo(".staggerAnimate4", 1, {y: screenHeight*9/2}, {y: 0, ease: Back.easeOut.config(0.75)}, 0.5);
 
-    // Each scene's duration is proportional to the difference between the screen and section height, (animation ends when section is vertically centered)
-	var scene1 = new ScrollMagic.Scene({triggerElement: "#trigger1", offset: 100, duration: (screenHeight - section1Height)/2})
+    // (FRAMEWORK) Each scene's duration is twice the screen height, so each ensuing scene must be offset by two screen's more than the previous scene
+    // The formula for each offset cycle is 2 full screens for duration + 1 full screen to offset each successive trigger = 3 full screens
+    sceneOffset = 0;
+    var scene1 = new ScrollMagic.Scene({triggerElement: "#trigger1", offset: sceneOffset + section1Height/2, duration: screenHeight*2})
+        .setPin("#section1")
         .setTween(tween1)
         .addIndicators({name: "staggering1"}) // add indicators (requires plugin)
         .addTo(controller);
-    var scene2 = new ScrollMagic.Scene({triggerElement: "#trigger2", offset: 100, duration: (screenHeight - section2Height)/2})
+    sceneOffset += screenHeight*3;
+    var scene2 = new ScrollMagic.Scene({triggerElement: "#trigger2", offset: sceneOffset + section2Height/2, duration: screenHeight*2})
+        .setPin("#section2")
         .setTween(tween2)
         .addIndicators({name: "staggering2"}) // add indicators (requires plugin)
         .addTo(controller);
-    var scene3 = new ScrollMagic.Scene({triggerElement: "#trigger3", offset: section3Height/2, duration: (screenHeight - section3Height)*2})
+    sceneOffset += screenHeight*3;
+    var scene3 = new ScrollMagic.Scene({triggerElement: "#trigger3", offset: sceneOffset + section3Height/2, duration: screenHeight*2})
         .setPin("#section3")
         .setTween(tween3)
-        .addIndicators({name: "pin3"}) // add indicators (requires plugin)
+        .addIndicators({name: "staggering3"}) // add indicators (requires plugin)
+        .addTo(controller);
+    sceneOffset += screenHeight*3;
+    var scene4 = new ScrollMagic.Scene({triggerElement: "#trigger4", offset: sceneOffset + section4Height/2, duration: screenHeight*2})
+        .setPin("#section4")
+        .setTween(tween4)
+        .addIndicators({name: "staggering4"}) // add indicators (requires plugin)
         .addTo(controller);
 
     // ------------------------------ Plane bezier animation (broken on mobile) ------------------------------ //

0f660d2961c79c80bfc9200be27cec9f556d735d
Jack Zhang <hijackz337@gmail.com>
2019-08-13 02:06:37 -0700

    Got pin and tween working for projects section

--- js/main.js
+++ js/main.js
@@ -24,12 +24,15 @@ $(function() {
     // Fade in the frontpage on startup
     var tween0 = TweenMax.fromTo("#front", 2, {autoAlpha: 0}, {autoAlpha: 1})
 
+    section1Height = document.getElementById("section1").clientHeight;
+    section2Height = document.getElementById("section2").clientHeight;
+    section3Height = document.getElementById("section3").clientHeight;
+    console.log(section3Height)
+
     // The animations for each profile section (start y's start at 3/2 and increment by 1 for each section, e.g. 3/2, 5/2, 7/2 ...)
     var tween1 = TweenMax.staggerFromTo(".staggerAnimate1", 1, {y: screenHeight*3/2}, {y: 0, ease: Back.easeOut.config(0.75)}, 0.5);
     var tween2 = TweenMax.staggerFromTo(".staggerAnimate2", 1, {y: screenHeight*5/2}, {y: 0, ease: Back.easeOut.config(0.75)}, 0.5);
-
-    section1Height = document.getElementById("section1").clientHeight;
-    section2Height = document.getElementById("section2").clientHeight;
+    var tween3 = TweenMax.staggerFromTo(".staggerAnimate3", 1, {y: screenHeight*7/2}, {y: 0, ease: Back.easeOut.config(0.75)}, 0.5);
 
     // Each scene's duration is proportional to the difference between the screen and section height, (animation ends when section is vertically centered)
 	var scene1 = new ScrollMagic.Scene({triggerElement: "#trigger1", offset: 100, duration: (screenHeight - section1Height)/2})
@@ -39,6 +42,11 @@ $(function() {
     var scene2 = new ScrollMagic.Scene({triggerElement: "#trigger2", offset: 100, duration: (screenHeight - section2Height)/2})
         .setTween(tween2)
         .addIndicators({name: "staggering2"}) // add indicators (requires plugin)
+        .addTo(controller);
+    var scene3 = new ScrollMagic.Scene({triggerElement: "#trigger3", offset: section3Height/2, duration: (screenHeight - section3Height)*2})
+        .setPin("#section3")
+        .setTween(tween3)
+        .addIndicators({name: "pin3"}) // add indicators (requires plugin)
         .addTo(controller);
 
     // ------------------------------ Plane bezier animation (broken on mobile) ------------------------------ //
